b3997fecd1fff049610b092b2069e8100511c09b
Cláudio Henrique <claudiohpo@gmail.com>
2024-04-24 23:09:24 -0300

    Inclusão dos exercícios

---
+++ Estrutura de Dados/1 - Introdução/Ex12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void troca(int a, int b) {
+    int c = a;
+    a = b;
+    b = c;
+}
+
+int main(void) {
+    int x = 5, y = 3;
+    troca(x, y);
+    printf("x: %d y: %d", x, y);
+}
