732b2943f811bddfa6b56091b5f5e8e5a453ce66
Cláudio Henrique <claudiohpo@gmail.com>
2024-09-21 23:24:39 -0300

    Create

--- Programação Orientada à Objetos/Exercícios/Apartamento/Main.java
+++ Programação Orientada à Objetos/Exercícios/Apartamento/Main.java
@@ -1,7 +1,3 @@
-/*Criar um programa Java que crie uma classe chamada apartamento.
-Esta classe em geral, possui quantidade de quartos, área total, 
-varanda gourmet, numero de banheiros, proprietário.
-Voces devem construir 2 objetos desta classe e apresentar o conteúdo deles na tela.  */
 package Apartamento;
 
 public class Main {
